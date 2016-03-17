86413e2b87f54757d3412063500b5d8922e489c6
Andrew Hoskins <andrew.hoskins16@gmail.com>
2016-03-17 10:28:06 -0600

    add comments and clean up

--- main.pl
+++ main.pl
@@ -1,63 +1,62 @@
+%%%%%%%%%%%%%%%%%% Helpers %%%%%%%%%%%%%%%%%%%%%%
+
 append([],L,L). % empty list and L is just L
 append([A|L],L1,[A|L2]) :- % append
   append(L,L1,L2).
 
-
-
-% i want to append one, two...do the second first
-
-
-% think of it as: when you execute a query, you are trying to prove
-%  a rule. and to prove, you have to dive into the rhs
 setMember(X,[X|_]).
 setMember(X,[_|Y]) :-
     setMember(X,Y).
 
-%%% Question 1
+%%%%%%%%%%%%%%%%%% Question 1 %%%%%%%%%%%%%%%%%%%%%%
 
-xreverse(List, Reverse) :- % public api
+% public api
+xreverse(List, Reverse) :-
   xreverse(List, [], Reverse).
 
 xreverse([], Result, Result). % base case
 xreverse([Head|Tail], Partial, Reverse) :- % worker
   xreverse(Tail, [Head|Partial], Reverse).
 
-%%% Question 2
+%%%%%%%%%%%%%%%%%% Question 2 %%%%%%%%%%%%%%%%%%%%%%
 
-notMember(_, []). % last element not the same
-notMember(X, [Y|Rest]) :-
-  X \== Y,
-  notMember(X, Rest).
+xunique([],[]). % base case
+xunique([Head|Tail], [Head|TailUnique]) :-
+  delete(Tail, Head, TailNew), % delete all Head from Tail, return TailNew
+  xunique(TailNew, TailUnique).
 
+% case when head is the target looking to delete
 delete([Head|Tail],Head,TailNew) :-
-  delete(Tail,Head,TailNew). % delete all Head from Tail
+  delete(Tail,Head,TailNew).
 
-delete([],_,[]). % base case
+% case base
+delete([],_,[]).
+
+% case when head is not the same as targt looking to delete, keep in TailNew
 delete([Head|Tail],E,[Head|TailNew]) :-
   Head \== E,
   delete(Tail,E,TailNew).
 
-xunique([],[]). % base case
-xunique([Head|Tail], [Head|TailUnique]) :-
-  delete(Tail, Head, TailNew), % delete all element Head from Tail to make TailNew
-  xunique(TailNew, TailUnique). % recursive step
-
-%%% Question 3
+%%%%%%%%%%%%%%%%%% Question 3 %%%%%%%%%%%%%%%%%%%%%%
 
 xunion(One, Two, Result) :-
   append(One, Two, AppendResult),
   xunique(AppendResult, Result).
 
-%%% Question 4
+%%%%%%%%%%%%%%%%%% Question 4 %%%%%%%%%%%%%%%%%%%%%%
 
-removeLast([Last], [], Last). % list has one element, result is empty
-removeLast([H|T], [H|WithoutLast], L) :- % list of two or more,
+% list has one element, result is empty
+removeLast([Last], [], Last).
+
+% list of two or more
+removeLast([H|T], [H|WithoutLast], L) :-
   removeLast(T, WithoutLast, L).
 
-%%% Question 5
+%%%%%%%%%%%%%%%%%% Question 5 %%%%%%%%%%%%%%%%%%%%%%
 
-% 5.1
+%%%%%%%%%%%%%%%%%% 5.1 %%%%%%%%%%%%%%%%%%%%%%
 
+% TODO: remove these
 node(a).
 node(b).
 node(c).
@@ -70,6 +69,7 @@ edge(c,a).
 edge(d,a).
 edge(a,e).
 
+% checks if param one is a subset of param two
 subset([], _).
 subset([X|Xs], Set) :-
   append(_, [X|Set1], Set),
@@ -82,7 +82,7 @@ clique(L) :-
 
 allConnected([]).
 allConnected([A|L]) :-
-  connect(A, L),
+  connect(A, L), % A connected to all others in L
   allConnected(L).
 
 % check if A is connected for every node in L
@@ -91,8 +91,9 @@ connect(A, [H|L]) :-
   (edge(A, H); edge(H, A)), % edge from either side
   connect(A, L). % and recure
 
-% 5.2
+%%%%%%%%%%%%%%%%%% 5.2 %%%%%%%%%%%%%%%%%%%%%%
 
+% add to result if is a clique
 allCliques([], _).
 allCliques([First|Rest], [First|Result]) :- % is a clique
   clique(First),
@@ -100,6 +101,7 @@ allCliques([First|Rest], [First|Result]) :- % is a clique
 allCliques([_|Rest], Result) :- % is not a clique
   allCliques(Rest, Result).
 
+% add to result if has no supersets
 allMaximalCliques([], _, []).
 allMaximalCliques([First|Rest], All, [First|Result]) :- % add, it's a maximal clique
   noSuperSets(First, All),
@@ -107,24 +109,34 @@ allMaximalCliques([First|Rest], All, [First|Result]) :- % add, it's a maximal cl
 allMaximalCliques([_|Rest], All, Result) :- % don't add, not a maximal clique
   allMaximalCliques(Rest, All, Result).
 
-noSuperSets(E, []).
+% check E against all in list param two and make sure it has no supersets
+noSuperSets(_, []).
 noSuperSets(E, [First|Rest]) :-
   (E == First ; notSubset(E, First)),
   noSuperSets(E, Rest).
 
-notSubset([], S) :- false. % empty is a subset of anything
-notSubset([First|Rest], S) :-
+% check that X not container in list param two
+notMember(_, []).
+notMember(X, [Y|Rest]) :-
+  X \== Y,
+  notMember(X, Rest).
+
+% check that param one is not a subset of param two
+notSubset([], _) :- false. % empty is a subset of anything
+notSubset([First|_], S) :-
   notMember(First, S).
-notSubset([First|Rest], S) :-
+notSubset([_|Rest], S) :-
   notSubset(Rest, S).
 
+% filter list of lists by having length exactly N
 ofLengthN([], _, []).
 ofLengthN([First|Rest], N, [First|Result]) :- % add to list
   length(First, N),
   ofLengthN(Rest, N, Result).
-ofLengthN([First|Rest], N, Result) :- % don't add to list
+ofLengthN([_|Rest], N, Result) :- % don't add to list
   ofLengthN(Rest, N, Result).
 
+% will generate all the subsets of a list
 allSubsets([], []).
 allSubsets([E|Tail], [E|NTail]):-
   allSubsets(Tail, NTail).
@@ -133,7 +145,7 @@ allSubsets([_|Tail], NTail):-
 
 maxclique(N, Cliques) :-
   findall(X, node(X), Nodes), % all the nodes
-  findall(Y, allSubsets(Nodes, Y), AllCandidates), % # find all perms of the nodes - 32 is all the perms
-  allCliques(AllCandidates, AllCliques), % # find all cliques -- 12 is length of AllCliques
-  allMaximalCliques(AllCliques, AllCliques, AllMaximalCliques), % working, i think
+  findall(Y, allSubsets(Nodes, Y), AllCandidates), % # find all perms of the nodes
+  allCliques(AllCandidates, AllCliques), % # find all cliques
+  allMaximalCliques(AllCliques, AllCliques, AllMaximalCliques), % all cliques without subset cliques
   ofLengthN(AllMaximalCliques, N, Cliques). % find all lists of length N
